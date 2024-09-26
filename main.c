043070c277cd4bd7588c2445c4998b3f0bf7cdf4
LightSulivy <bast91.teisseire@gmail.com>
2024-09-26 20:41:28 +0200

    Affichage du tableau avec les numéros de colonnes

--- main.c
+++ main.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+
+    //Values pour la grille
+
+    unsigned int tailleHauteur = 6;
+    unsigned int tailleLarger = 7;
+
+    //+1 pour le \0
+    unsigned int hauteur = tailleHauteur;
+    unsigned int largeur = (4*tailleLarger)+2;
+
+    char *ligne = (char*)malloc(largeur*sizeof(char));
+    char **plateau = (char**)malloc(hauteur*sizeof(*ligne));
+
+    for (int h = 0; h < hauteur; h++) {
+        char *temp = (char*)malloc(largeur*sizeof(char));
+        plateau[h] = temp;
+    }
+
+    for (int h = 0; h < hauteur; h++) {
+        for (int l = 0; l < largeur; l++) {
+            plateau[h][l] = ' ';
+            if(l%4==0) plateau[h][l] = '|';
+            if(l==largeur-1) plateau[h][l] = '\0';
+        }
+    }
+
+    for (int h = 0; h < hauteur; h++) {
+        int l = 0;
+        while (plateau[h][l]!='\0'){
+            printf("%c", plateau[h][l]);
+            l++;
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < tailleLarger; i++) {
+        printf(" ");
+        printf("%03d", i);
+    }
+
     return 0;
 }
